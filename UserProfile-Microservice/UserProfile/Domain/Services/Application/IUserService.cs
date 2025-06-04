8777386006b9e99b4b1cecd2dfa11e226cdc7aea
MRamirez202210582 <U202210582@upc.edu.pe>
2025-06-04 00:29:54 -0500

    feat(main):add user domain services

---
+++ UserProfile-Microservice/UserProfile/Domain/Services/Application/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using DittoBox.API.UserProfile.Domain.Models.Entities;
+
+namespace DittoBox.API.UserProfile.Domain.Services.Application
+{
+    public interface IUserService
+    {
+        public Task<User> CreateUser(string username, string email, string password);
+        public Task<User?> GetUser(int id);
+        public Task DeleteUser(int id);
+        public string EncryptPassword(string password);
+        public Task UpdateUser(User user);
+		public Task<IEnumerable<User>> GetUsers();
+        public Task<string?> Login(string email, string password);
+        public Task<User?> GetUserByEmail(string email);
+		public Task<User?> GetUserByUsername(string username);
+
+    }
+}
